147fa90baca70552ffd78b6fc30429f432ddc7b1
Raphael Mitsch <laarem33@hotmail.com>
2015-05-29 14:36:24 +0200

    Proceeded work on Data Generation view.

--- src/control/GenerationController.java
+++ src/control/GenerationController.java
@@ -1,8 +1,10 @@
  package control;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 import java.util.ResourceBundle;
 
 import org.controlsfx.control.RangeSlider;
@@ -26,11 +28,11 @@ public class GenerationController extends Controller
 	private GridPane parameterConfiguration_gridPane;
 	
 	@FXML
-	private BarChart<String, Float> alpha_barchart;
+	private BarChart<String, Integer> alpha_barchart;
 	@FXML
-	private BarChart<String, Float> eta_barchart;
+	private BarChart<String, Integer> eta_barchart;
 	@FXML
-	private BarChart<String, Float> kappa_barchart;
+	private BarChart<String, Integer> kappa_barchart;
 	
 	@FXML
 	private TextField alpha_min_textfield;
@@ -47,6 +49,8 @@ public class GenerationController extends Controller
 	
 	@FXML
 	private TextField numberOfDatasets_textfield;
+	@FXML
+	private TextField numberOfDivisions_textfield;
 	
 	@FXML
 	private Button parameterCoupling_button;
@@ -58,6 +62,7 @@ public class GenerationController extends Controller
 	private Map<String, VBox> vBoxes;
 	
 	private int numberOfDatasetsToGenerate;
+	private int numberOfDivisions;
 	
 	
 	@Override
@@ -69,13 +74,6 @@ public class GenerationController extends Controller
 		rangeSliders	= new HashMap<String, RangeSlider>();
 		vBoxes			= new HashMap<String, VBox>();
 		
-		try {
-			numberOfDatasetsToGenerate = Integer.parseInt(numberOfDatasets_textfield.getText());
-		}
-		catch (NumberFormatException e) {
-			e.printStackTrace();
-		}
-		
 		// Init UI elements.
 		initUIElements();
 	}
@@ -86,6 +84,9 @@ public class GenerationController extends Controller
 	 */
 	private void initUIElements() 
 	{
+		// Parse pre-configured textfield valus.
+		initTextFields();
+		
 		// Remove deprecated UI elements in parameter configuration grid pane.
 		int vboxCount = 0;
 		for (Object child : parameterConfiguration_gridPane.getChildren().toArray()) {
@@ -134,6 +135,46 @@ public class GenerationController extends Controller
 	    }
 		
 		// Init range sliders.
+		initRangeSliders();
+		
+		// Init combobox.
+		initComboBoxes();
+	}
+	
+	/**
+	 * Initialize ComboBox UI elements.
+	 */
+	private void initComboBoxes()
+	{
+		sampling_combobox.getItems().clear();
+		sampling_combobox.getItems().add("Random");
+		sampling_combobox.getItems().add("Cartesic");
+		sampling_combobox.getItems().add("Latin Hypercube");
+		sampling_combobox.setValue("Random");
+	}
+	
+	/**
+	 * Initialize TextField UI elements.
+	 */
+	private void initTextFields()
+	{
+		try {
+			numberOfDatasetsToGenerate	= Integer.parseInt(numberOfDatasets_textfield.getText());
+			numberOfDivisions			= Integer.parseInt(numberOfDivisions_textfield.getText());
+			
+			numberOfDatasets_textfield.setDisable(true);
+		}
+		
+		catch (NumberFormatException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Initialize RangeSlider UI elements.
+	 */
+	private void initRangeSliders()
+	{
 		rangeSliders.put("alpha", new RangeSlider());
 		rangeSliders.put("eta", new RangeSlider());
 		rangeSliders.put("kappa", new RangeSlider());
@@ -151,12 +192,6 @@ public class GenerationController extends Controller
 			
 			addEventHandlerToRangeSlider(rs, entry.getKey());
 		}
-		
-		// Init combobox.
-		sampling_combobox.getItems().clear();
-		sampling_combobox.getItems().add("Random");
-		sampling_combobox.getItems().add("Cartesic");
-		sampling_combobox.getItems().add("Hypercube");
 		
 		// Add to respective parents.
 		vBoxes.get("alpha").getChildren().add(rangeSliders.get("alpha"));
@@ -240,6 +275,49 @@ public class GenerationController extends Controller
 		try {
 			numberOfDatasetsToGenerate = Integer.parseInt(numberOfDatasets_textfield.getText());
 			System.out.println("New number of datasets to generate: " + numberOfDatasetsToGenerate);
+			
+			switch(sampling_combobox.getValue())
+			{
+				// If sampling mode == random: Number of datasets to generate equals number of divisions for each parameter.
+				case "Random":
+					numberOfDivisions = numberOfDatasetsToGenerate;
+					numberOfDivisions_textfield.setText(String.valueOf(numberOfDivisions));
+				break;
+				
+				case "Cartesic":
+				break;
+				
+				case "Hypercube":
+				break;
+			}			
+		}
+		
+		catch (NumberFormatException ex) {
+			System.out.println("Only numbers are accepted. Correct input.");
+		}
+	}
+	
+	@FXML
+	private void updateNumberOfDivisions(ActionEvent e)
+	{
+		try {
+			numberOfDivisions = Integer.parseInt(numberOfDivisions_textfield.getText());
+			System.out.println("New number of divisions: " + numberOfDivisions);
+			
+			switch(sampling_combobox.getValue())
+			{
+				// If sampling mode == random: Number of datasets to generate equals number of divisions for each parameter.				
+				case "Random":
+					numberOfDatasetsToGenerate = numberOfDivisions;
+					numberOfDatasets_textfield.setText(String.valueOf(numberOfDatasetsToGenerate));
+				break;
+				
+				case "Cartesic":
+				break;
+				
+				case "Hypercube":
+				break;
+			}
 		}
 		
 		catch (NumberFormatException ex) {
@@ -252,37 +330,97 @@ public class GenerationController extends Controller
 	 */
 	private void generateParameterValues()
 	{
-		// @todo Generate parameter vaulues in dependency from sampling mode.
-		switch (sampling_combobox.getValue())
-		{
-			case "Random":
-				System.out.println("Randomly generating parameter data.");
-				
-				for (int i = 0; i < numberOfDatasetsToGenerate; i++) {
-					// @todo Here: Generate random parameter values.
-				}
-			break;
-			
-			case "Cartesic":
-				System.out.println("Sampling mode 'Cartesic' currently not supported.");
-			break;
-			
-			case "Hypercube":
-				System.out.println("Sampling mode 'Hypercube' currently not supported.");
-			break;			
+		/**
+		 * Initialize variables for storage of the actual parameter data and prepare the histogram bins aggregating them.  
+		 */
+		
+		// Init storage.
+		Map<String, ArrayList<Double>> parameterValues = new HashMap<String, ArrayList<Double>>();
+		parameterValues.put("alpha", new ArrayList<Double>(numberOfDivisions));
+		parameterValues.put("eta", new ArrayList<Double>(numberOfDivisions));
+		parameterValues.put("kappa", new ArrayList<Double>(numberOfDivisions));
+		
+		// Init bins.
+		final int numberOfBins 					= 100;
+		Map<String, int[]> parameterBinLists	= new HashMap<String, int[]>();
+		parameterBinLists.put("alpha", new int[numberOfBins]);
+		parameterBinLists.put("eta", new int[numberOfBins]);
+		parameterBinLists.put("kappa", new int[numberOfBins]);
+		
+		/**
+		 * Generate numberOfDivisions values for each parameter.
+		 */
+		
+		// Init random number generator.
+		 Random randomGenerator = new Random();
+		// Generated values are allowed to be up to rangeSlider.getHighValue() and as low as rangeSlider.getLowValue().
+		double intervalAlpha 	= rangeSliders.get("alpha").getHighValue() - rangeSliders.get("alpha").getLowValue();
+		double intervalEta		= rangeSliders.get("eta").getHighValue() - rangeSliders.get("eta").getLowValue();
+		double intervalKappa	= rangeSliders.get("kappa").getHighValue() - rangeSliders.get("kappa").getLowValue();
+
+		// Generate random parameter values.		
+		for (int i = 0; i < numberOfDivisions; i++) {
+			parameterValues.get("alpha").add(rangeSliders.get("alpha").getLowValue() + randomGenerator.nextFloat() * intervalAlpha);
+//			System.out.println(parameterValues.get("alpha").get(parameterValues.get("alpha").size() - 1));
+			parameterValues.get("eta").add(rangeSliders.get("eta").getLowValue() + randomGenerator.nextFloat() * intervalEta);
+			parameterValues.get("kappa").add(rangeSliders.get("kappa").getLowValue() + randomGenerator.nextFloat() * intervalKappa);
 		}
-		// Construct data for histogram of generated alpha parameters.
+		
+		/**
+		 * @todo Bin data for use in histograms/scented widgets.
+		 */
+		// @todo Use .getHighValue() instead of .getMax()? What's more appropriate?
+		double binInterval		= (rangeSliders.get("alpha").getMax() - rangeSliders.get("alpha").getMin()) / numberOfBins;
+		// Bin data.
+		for (Map.Entry<String, ArrayList<Double>> entry : parameterValues.entrySet()) {
+			// Check every value and assign it to the correct bin.
+			for (double value : entry.getValue()) {
+				// Increment content of corresponding bin.
+				parameterBinLists.get(entry.getKey())[(int) (value / binInterval)]++;
+			}
+		}
+		
+		for (int x : parameterBinLists.get("alpha")) {
+			System.out.println(x);
+		}
+		System.out.println();
+		/**
+		 * Transfer data to scented widgets.
+		 */
+		
+		// Clear old data.
 		alpha_barchart.getData().clear();
-		final XYChart.Series<String, Float> alpha_series = new XYChart.Series<String, Float>();
-		alpha_series.setName("alpha parameter data");
-		for (int i = 0; i < rangeSliders.get("alpha").getMax(); i++) {
+		
+		// @todo Find way to prevent BarChart from dynamically adjusting bar height per unit.
+		// @todo Move BarCharts slightly to the left.
+		
+		// Add data to barcharts.
+		
+		
+		// Add data series to barcharts.
+		alpha_barchart.getData().add(generateParameterHistogramDataSeries("alpha", parameterBinLists));
+	}
+	
+	private XYChart.Series<String, Integer> generateParameterHistogramDataSeries(String key, Map<String, int[]> parameterBinLists)
+	{
+		final XYChart.Series<String, Integer> data_series = new XYChart.Series<String, Integer>();
+		System.out.println(rangeSliders.get(key).getMin() + " - " + rangeSliders.get(key).getMax());
+		data_series.getData().add(new XYChart.Data<String, Integer>("-1", 10));
+		for (int i = (int) rangeSliders.get(key).getMin(); i < rangeSliders.get(key).getMax(); i++) {
+			int binContent = parameterBinLists.get(key)[i];
+			System.out.println(binContent);
+			data_series.getData().add(new XYChart.Data<String, Integer>(String.valueOf(i), binContent ));
 			// If in defined range: Output parameter counts.
-			if (i >= rangeSliders.get("alpha").getLowValue() && i <= rangeSliders.get("alpha").getHighValue())
-				alpha_series.getData().add(new XYChart.Data<String, Float>(String.valueOf(i), (float)(i * 10) ));
+//			if (i >= rangeSliders.get(key).getLowValue() && i <= rangeSliders.get(key).getHighValue()) {
+//				data_series.getData().add(new XYChart.Data<String, Float>(String.valueOf(i), (float)(i * 10) ));
+//				
+//			}
 			// Otherwise: Output 0.
-			else
-				alpha_series.getData().add(new XYChart.Data<String, Float>(String.valueOf(i), (float)0 ));
+//			else {
+//				data_series.getData().add(new XYChart.Data<String, Float>(String.valueOf(i), (float)0 ));
+//			}
 		}
-		alpha_barchart.getData().add(alpha_series);
+		
+		return data_series;
 	}
 }
