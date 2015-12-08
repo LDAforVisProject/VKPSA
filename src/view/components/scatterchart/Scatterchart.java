da39602e757455e622cd71a5760e33cf2b497648
Raphael Mitsch <laarem33@hotmail.com>
2015-12-08 19:12:49 +0100

    Minor adaptions for future generalization of ParameterSpaceScatterchart. Next: Density heatmap (including options) and single selection handler for PSS; then: move options panel into popup.

--- src/view/components/scatterchart/Scatterchart.java
+++ src/view/components/scatterchart/Scatterchart.java
@@ -2,6 +2,7 @@ package view.components.scatterchart;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Set;
@@ -10,6 +11,7 @@ import model.LDAConfiguration;
 
 import com.sun.javafx.charts.Legend;
 
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.chart.NumberAxis;
@@ -18,7 +20,9 @@ import javafx.scene.chart.XYChart;
 import javafx.scene.chart.XYChart.Series;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
+import javafx.util.Pair;
 import view.components.VisualizationComponent;
+import view.components.legacy.mdsScatterchart.DataPointState;
 
 public abstract class Scatterchart extends VisualizationComponent
 {
@@ -243,5 +247,233 @@ public abstract class Scatterchart extends VisualizationComponent
 		boolean nodeYWithinBounds = node.getLayoutY() <= maxY && node.getLayoutY() >= minY;
 		
 		return nodeXWithinBounds && nodeYWithinBounds;
+	}
+	
+	@Override
+	public void processSelectionManipulationRequest(double minX, double minY, double maxX, double maxY)
+	{
+		// Check if settings icon was used. Workaround due to problems with selection's mouse event handling. 
+		if (minX == maxX && minY == maxY) {
+			Pair<Integer, Integer> offsets = provideOffsets();
+			analysisController.checkIfSettingsIconWasClicked(minX + offsets.getKey(), minY + offsets.getValue(), "settings_mds_icon");
+		}
+		
+		// Set of data points to add to selection. 
+		Set<XYChart.Data<Number, Number>> pointsToAddToSelection		= new HashSet<XYChart.Data<Number,Number>>();
+		// Set of data points to remove from selection.
+		Set<XYChart.Data<Number, Number>> pointsToRemoveFromSelection	= new HashSet<XYChart.Data<Number,Number>>();
+		
+		// If control is not down: Ignore selected points, add all non-selected in chosen area.
+		if (!isCtrlDown) {
+			// Iterate over all filtered points.
+			for (XYChart.Data<Number, Number> datapoint : inactiveDataSeries.getData()) {
+				
+				// Point was selected - add to sets of selected indices and selected points.
+				if (	isNodeWithinBounds(datapoint.getNode(), minX, minY, maxX, maxY) &&
+						!activePoints.containsKey((int)datapoint.getExtraValue())
+					) {
+					// Set dirty flags.
+					changeInSelectionDetected				= true;
+					changeInSelectionDetected_localScope	= true;
+					
+					// Update collection of selected points.
+					activePoints.put((int)datapoint.getExtraValue(), datapoint);
+					inactivePoints.remove((int)datapoint.getExtraValue());
+					
+					// Update set of values added to selection in this action.
+					pointsToAddToSelection.add(datapoint);
+					
+					// Mark data point as manipulated in this step.
+					pointsManipulatedInCurrSelectionStep.add((int) datapoint.getExtraValue());
+				}
+			}
+
+			// Iterate over all selected points.
+			for (XYChart.Data<Number, Number> datapoint : activeDataSeries.getData()) {
+				if (	!isNodeWithinBounds(datapoint.getNode(), minX, minY, maxX, maxY) 	&&
+						activePoints.containsKey((int)datapoint.getExtraValue())			&&
+						pointsManipulatedInCurrSelectionStep.contains((int) datapoint.getExtraValue())
+					) {
+					// Set dirty flags.
+					changeInSelectionDetected				= true;
+					changeInSelectionDetected_localScope	= true;
+					
+					// Update collection of selected points.
+					activePoints.remove((int)datapoint.getExtraValue());
+					
+					// Update set of values removed from selection in this action.
+					pointsToRemoveFromSelection.add(datapoint);
+				}
+			}
+		}
+		
+		// Else if control is down: Remove selected points in chose area from selection (in data series 0).
+		else {
+			// Iterate over selected points.
+			for (XYChart.Data<Number, Number> datapoint : activeDataSeries.getData()) {
+				// Point was deselected - remove from of selected points and selected indices.
+				if (isNodeWithinBounds(datapoint.getNode(), minX, minY, maxX, maxY) &&
+					activePoints.containsKey((int)datapoint.getExtraValue())
+				) {
+					// Set dirty flags.
+					changeInSelectionDetected				= true;
+					changeInSelectionDetected_localScope	= true;
+					
+					// Update collection of selected points.
+					inactivePoints.put((int)datapoint.getExtraValue(), datapoint);
+					activePoints.remove((int)datapoint.getExtraValue());
+					
+					// Update set of values removed from selection in this action.
+					pointsToRemoveFromSelection.add(datapoint);			
+					
+					// Mark data point as manipulated in this step.
+					pointsManipulatedInCurrSelectionStep.add((int) datapoint.getExtraValue());
+				}
+			}
+			
+			// Iterate over all filtered points.
+			for (XYChart.Data<Number, Number> datapoint : inactiveDataSeries.getData()) {
+				// Point was deselected - remove from of selected points and selected indices.
+				if (!isNodeWithinBounds(datapoint.getNode(), minX, minY, maxX, maxY)	&&
+					!activePoints.containsKey((int)datapoint.getExtraValue())			&&
+					pointsManipulatedInCurrSelectionStep.contains((int) datapoint.getExtraValue())
+				) {
+					// Set dirty flags.
+					changeInSelectionDetected				= true;
+					changeInSelectionDetected_localScope	= true;
+					
+					// Update collection of selected points.
+					activePoints.remove((int)datapoint.getExtraValue());
+					inactivePoints.remove((int)datapoint.getExtraValue());
+					
+					// Update set of values removed from selection in this action.
+					pointsToAddToSelection.add(datapoint);						
+				}
+			}
+		}
+		
+		// Incorporate identified selection changes into data point's series associations.
+		if (changeInSelectionDetected) {
+			// Add newly selected data points to selection.
+			for (XYChart.Data<Number, Number> newlySelectedDataPoint : pointsToAddToSelection) {
+				// Change selection status.
+				changeDataPointSelectionStatus(newlySelectedDataPoint, true);
+				
+				// Update collection of selected points.
+				activePoints.put((int)newlySelectedDataPoint.getExtraValue(), newlySelectedDataPoint);
+			}
+			
+			// Remove newly de-selected data points to selection.
+			for (XYChart.Data<Number, Number> newlyDeselectedDataPoint : pointsToRemoveFromSelection) {
+				// Change selection status.
+				changeDataPointSelectionStatus(newlyDeselectedDataPoint, false);
+			}
+			
+    		// Reset flag.
+    		changeInSelectionDetected = false;
+    		
+    		// Clear set of active points.
+//    		pointsManipulatedInCurrSelectionStep.clear();
+//    		activePoints.clear();
+    		
+    		// Update data series names with number of datasets.
+    		updateLegendLabels();
+		}
+	}
+	
+	/**
+	 * Changes selection status of a point in filtered/selected data series.
+	 * @param data
+	 * @param selected Indicates that data point should be selected, if this is true.
+	 */
+	private void changeDataPointSelectionStatus(XYChart.Data<Number, Number> data, boolean selected)
+	{
+		// Copy data point in question.
+		XYChart.Data<Number, Number> dataCopy = new XYChart.Data<Number, Number>(data.getXValue(), data.getYValue());
+		dataCopy.setExtraValue(data.getExtraValue());
+		
+		if (selected) {
+			// Remove from filteredDataSeries.
+			inactiveDataSeries.getData().remove(data);
+			
+			// Add copy to selectedDataSeries.
+			if (!activeDataSeries.getData().contains(data)) {
+				activeDataSeries.getData().add(dataCopy);
+
+				// Add listener for single selection mode.
+	    		addSingleSelectionModeMouseListenerToNode(dataCopy, DataPointState.ACTIVE);
+			}
+		}
+		
+		else {
+			// Remove from selectedDataSeries.
+			activeDataSeries.getData().remove(data);
+			
+			// Add copy to filteredDataSeries.
+			if (!inactiveDataSeries.getData().contains(data)) {
+				inactiveDataSeries.getData().add(dataCopy);
+				
+				// Add listener for single selection mode.
+	    		addSingleSelectionModeMouseListenerToNode(dataCopy, DataPointState.INACTIVE);
+			}
+		}
+	}
+	
+	
+	/**
+	 * Assigns mouse listener for single selection node to data point's node.
+	 * @param dataPoint
+	 */
+	private void addSingleSelectionModeMouseListenerToNode(XYChart.Data<Number, Number> dataPoint, DataPointState state)
+	{
+		switch (state)
+		{
+			case INACTIVE:
+				dataPoint.getNode().setOnMouseClicked(new EventHandler<MouseEvent>()
+				{
+				    @Override
+				    public void handle(MouseEvent mouseEvent)
+				    {       
+				    	// If control is not down: Add to selection.
+				    	if (!mouseEvent.isControlDown()) {
+					    	if (!activePoints.containsKey(dataPoint.getExtraValue())) {
+					    		// Update collection of selected points.
+								activePoints.put((int)dataPoint.getExtraValue(), dataPoint);
+
+								// Change data point selection status.
+								changeDataPointSelectionStatus(dataPoint, true);
+					    		
+								// Refresh other charts.
+					    		analysisController.integrateMDSSelection(activePoints.keySet(), false);
+					    	}
+				    	}
+				    	else
+				    		System.out.println("blub");
+				    }
+				});
+			break;
+			
+			case ACTIVE:
+	        	dataPoint.getNode().setOnMouseClicked(new EventHandler<MouseEvent>()
+    			{
+    			    @Override
+    			    public void handle(MouseEvent mouseEvent)
+    			    {       
+    			    	System.out.println("in event");
+    			    	// If control is down: Remove from selection.
+    			    	if (isCtrlDown || mouseEvent.isControlDown()) {
+				    		// Update collection of selected points.
+				    		activePoints.remove(dataPoint.getExtraValue());
+							
+							// Change data point selection status.
+							changeDataPointSelectionStatus(dataPoint, false);
+							
+							// Refresh other charts.
+				    		analysisController.integrateMDSSelection(activePoints.keySet(), false);
+    			    	}
+    			    }
+    			});
+			break;
+		}
 	}
 }
